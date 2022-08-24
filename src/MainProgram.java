109615b9da06b267dd9262d6a5f5d8b16400f225
moushaya <hayamou2006@yahoo.com>
2022-08-24 15:04:55 +0200

    Jasmina loves herself way too much

---
+++ src/MainProgram.java
@@ -0,0 +1,8 @@
+public class MainProgram {
+    public static void main (String[] args){
+        System.out.println("Hello World :0");
+        System.out.print("jasmina ist cool");
+        System.out.print("jasmina ist cool");
+        System.out.println("Hello World :0");
+    }
+}
